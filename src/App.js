334fa8043e264baec6d0b8da5787dfb6f5196c59
muhammad-junaid1 <mjunaid.swe@gmail.com>
2022-01-09 21:43:33 +0500

    create Button component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import "./App.css";
+import Button from "./components/main-components/Button";
 
 function App() {
   return (
     <>
-      <div className="wrapper">
-        <h1>Hello React!</h1>
+      <div className="wrapper" style={{color: "white"}}>
+        <Button type="primary">Button</Button>
+        <Button type="secondary">Button</Button>
+        <Button type="secondary" color="green">Button</Button>
+        <Button type="secondary" color="grey">Button</Button>
+        <Button type="primary" size="small" color="red">Button</Button>
+        <Button type="primary" size="small" color="green">Button</Button>
+        <Button type="primary" size="small" color="violet">Button</Button>
+        <Button type="primary" size="small" color="grey">Button</Button>
+        <Button isDisabled={true}>Disabled</Button>
       </div>
     </>
   );
---
+++ src/components/main-components/Button.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "../../css/Button.css";
+
+const TYPES = ["primary", "secondary"];
+const COLORS = ["green", "voilet", "grey", "red"];
+const SIZES = ["small", "normal"];
+
+const Button = ({ children, type, color, size, isDisabled }) => {
+  let getType = "btn-primary";
+  let getColor = `btn--${type ? type : "primary"}-color`;
+  let getSize = `btn--normal-size`;
+  if (type) {
+    getType = TYPES.includes(type) ? `btn-${type}` : "btn-primary";
+  }
+  if (color) {
+    getColor = COLORS.includes(color)
+      ? `btn--${color}-color`
+      : `btn--${type}-color`;
+  }
+  if (size) {
+    getSize = SIZES.includes(size) ? `btn--${size}-size` : `btn--normal-size`;
+  }
+  return (
+    <>
+      <button
+        className={`btn ${getType} ${getColor} ${getSize}`}
+        disabled={isDisabled ? true : false}
+      >
+        {children}
+      </button>
+    </>
+  );
+};
+
+export default Button;
