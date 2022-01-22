7394e2e337c6ada5ea55c999a68b270e93e16ba7
muhammad-junaid1 <mjunaid.swe@gmail.com>
2022-01-22 23:22:11 +0500

    crypto section done

--- src/components/pages/components/CryptoRow.jsx
+++ src/components/pages/components/CryptoRow.jsx
@@ -1,45 +1,80 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import "../css/CryptoRow.css";
 import SVGIcons from "../../SvgIcons";
 import CryptoBet from "./CryptoBet";
 import { AllRoutesContext } from "../../Main";
 
-const CryptoRow = ({coin, endTime, prices, bet}) => {
-    const {showBetsContainer, isMobile} = useContext(AllRoutesContext);
-    const Icon = SVGIcons.CryptoIcons[coin.icon];
+const CryptoRow = ({ coin, endTime, prices, bet }) => {
+  const { showBetsContainer, isMobile } = useContext(AllRoutesContext);
+  const Icon = SVGIcons.CryptoIcons[coin.icon];
   return (
     <>
       <tr>
         <td>
           <div>
-              <div className="coin">
-                    <Icon/>
-                    <p>BTC touch {coin.price} {!isMobile && <br />} by {coin.date}</p>
-              </div>
+            <div className="coin">
+              <Icon />
+              <p>
+                BTC touch {coin.price} {!isMobile && <br />} by {coin.date}
+              </p>
+            </div>
           </div>
         </td>
         <td>
-          <div><div className="endtime" style={{marginRight: showBetsContainer ? "1.5rem" : ""}}>
-              <p>{endTime[0]}</p>
-              <p>{endTime[1]}</p>
-          </div> </div>
+          <div style={{ display: isMobile ? "block" : "" }}>
+            <div
+              className="endtime"
+              style={{ marginRight: showBetsContainer ? "1.5rem" : "" }}
+            >
+              {isMobile ? (
+                <p>
+                  Market ends at {endTime[0]} {endTime[1]}
+                </p>
+              ) : (
+                <>
+                  <p>{endTime[0]}</p>
+                  <p>{endTime[1]}</p>
+                </>
+              )}
+            </div>
+          </div>
         </td>
+        {isMobile &&
         <td>
           <div>
-              <div className="crypto-bets">
-                  <CryptoBet type="moon" {...bet}/>
-                  <CryptoBet type="rekt" {...bet}/>
-              </div>
+            <div className="prices">
+              <p>
+                <span>Locked Price</span> {prices.locked}
+              </p>
+              <p>
+                <span>Closed Price</span> {prices.closed}
+              </p>
+            </div>
           </div>
         </td>
+        }
         <td>
-            <div>
-                <div className="prices">
-                    <p><span>Locked Price</span> {prices.locked}</p>
-                    <p><span>Closed Price</span> {prices.closed}</p>
-                </div>
+          <div>
+            <div className="crypto-bets">
+              <CryptoBet type="moon" {...bet} />
+              <CryptoBet type="rekt" {...bet} />
             </div>
+          </div>
         </td>
+        {!isMobile &&
+        <td>
+          <div>
+            <div className="prices">
+              <p>
+                <span>Locked Price</span> {prices.locked}
+              </p>
+              <p>
+                <span>Closed Price</span> {prices.closed}
+              </p>
+            </div>
+          </div>
+        </td>
+        }
       </tr>
     </>
   );
