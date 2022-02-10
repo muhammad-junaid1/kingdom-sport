96d185327e8bd29fbe3fc3cc58785225cb00f3fd
muhammad-junaid1 <mjunaid.swe@gmail.com>
2022-02-10 22:03:48 +0500

    graph done

--- src/components/pages/components/CoinSection.jsx
+++ src/components/pages/components/CoinSection.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useContext } from "react";
 import BreadCrumbs from "./BreadCrumbs";
 import SVGIcons from "../../SvgIcons";
 import Button from "../../Button";
@@ -6,37 +6,50 @@ import "../css/CoinSection.css";
 import CryptoBet from "./CryptoBet";
 import { ContentRoutesContext } from "../../Main";
 
+// Graph
+import Chart from "chart.js/auto";
+import { Line } from "react-chartjs-2";
+
 const CoinSection = ({ coin, endTime, bet, prices }) => {
   const Icon = SVGIcons.CryptoIcons[coin.icon];
-  const { isMobile, showBetsContainer} = useContext(ContentRoutesContext);
+  const { isMobile, showBetsContainer } = useContext(ContentRoutesContext);
 
   // Graph section tabs
-  const [activeTab, setActiveTab ] = useState("All time");
+  const [activeTab, setActiveTab] = useState("All time");
   const graphTabs = ["All time", "24h", "7d", "30d"];
   return (
     <>
-      <div className="coin-section" style={{width: showBetsContainer ? "100%" : ""}}>
+      <div
+        className="coin-section"
+        style={{ width: showBetsContainer ? "100%" : "" }}
+      >
         <div className="coin-section__header">
           <BreadCrumbs items={["CRYPTO", coin.details()]} />
         </div>
         <div className="coin-section__content">
           <div className="coin-content__container">
             <div className="left">
-              {!isMobile && <p className="coin-content__header">
-                <Icon color="#743EE8" /> {coin.text}
-              </p>
-              }
+              {!isMobile && (
+                <p className="coin-content__header">
+                  <Icon color="#743EE8" /> {coin.text}
+                </p>
+              )}
               {isMobile ? (
-                <p className="coin-content__header"><Icon/> {coin.details()}</p>
+                <p className="coin-content__header">
+                  <Icon /> {coin.details()}
+                </p>
               ) : (
                 <h2 className="coin-content__details">{coin.details()}</h2>
               )}
               <p className="coin-content__endtime">
-              {isMobile ? `Market ends at ${endTime[0]} ${endTime[1]}` : <>
-                <span className="fw-bold">END TIME</span> {endTime[0]}{" "}
-                {endTime[1]}
-                </>
-              }
+                {isMobile ? (
+                  `Market ends at ${endTime[0]} ${endTime[1]}`
+                ) : (
+                  <>
+                    <span className="fw-bold">END TIME</span> {endTime[0]}{" "}
+                    {endTime[1]}
+                  </>
+                )}
               </p>
               <div className="coin-content__prices">
                 <p className="prices__locked">
@@ -57,16 +70,50 @@ const CoinSection = ({ coin, endTime, bet, prices }) => {
             </div>
           </div>
           <div className="coin-content__graph">
-          <div className="coin-graph__header">
-          <div className="coin-graph__tabs">
-              {graphTabs.map((tab, index) => {
-                return <Button key={index} onClick={() => setActiveTab(tab)} {...(activeTab===tab ? {type: "primary"} : null)}>{tab}</Button>
-              })}
-          </div>
-          </div>
-          <div className="coin-graph__body">
-              <h1>Graph here..</h1>
-          </div>
+            <div className="coin-graph__header">
+              <div className="coin-graph__tabs">
+                {graphTabs.map((tab, index) => {
+                  return (
+                    <Button
+                      key={index}
+                      onClick={() => setActiveTab(tab)}
+                      {...(activeTab === tab ? { type: "primary" } : null)}
+                    >
+                      {tab}
+                    </Button>
+                  );
+                })}
+              </div>
+            </div>
+            <div className="coin-graph__body">
+              <Line
+              type="line"
+                options={
+                  {
+                    plugins: {
+                      tooltip: false,
+                      legend: {
+                        display: false
+                      }
+                    },
+                  }
+                }
+                data={{
+                  labels: [
+                    2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022,
+                  ],
+                  datasets: [
+                    {
+                      fill: true,
+                      backgroundColor: "#23582c",
+                      borderColor: "#32D74B",
+                      borderWidth: 3,
+                      data: [0, 0, 5, 0, 30, 25, 10, 70, 0],
+                    },
+                  ],
+                }}
+              />
+            </div>
           </div>
         </div>
       </div>
